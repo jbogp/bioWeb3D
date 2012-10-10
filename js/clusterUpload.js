7f057f7d68c9af100625f0ae816c81c2ad00173b
Jean-Baptiste Pettit <jbpettit@vanilla.ebi.ac.uk>
2012-10-10 16:59:44 +0100

    changes 10/10

--- js/clusterUpload.js
+++ js/clusterUpload.js
@@ -31,9 +31,20 @@ function getClusterAsText(readFile,datasetId) {
 		$("#contentClustAccordion"+datasetNum+"-"+clustNum+"").append("<div style='display:inline' id='buttonsClustAccordion"+datasetNum+"-"+clustNum+"'></div>");
 		//Creating World buttons		
 		for(var j=0;j<worlds.length;j++) {
-			$("#buttonsClustAccordion"+datasetNum+"-"+clustNum+"").append("<input type='checkbox' id='worldSelectClustAccordion"+datasetNum+"-"+clustNum+"-"+j+"' /><label for='worldSelectClustAccordion"+datasetNum+"-"+clustNum+"-"+j+"'>world "+j+"</label>");
+			$("#buttonsClustAccordion"+datasetNum+"-"+clustNum+"").append("<input type='checkbox' class='worldSelect"+datasetNum+"-"+j+"' id='worldSelectClustAccordion"+datasetNum+"-"+clustNum+"-"+j+"' /><label for='worldSelectClustAccordion"+datasetNum+"-"+clustNum+"-"+j+"'>world "+j+"</label>");
 			$("#worldSelectClustAccordion"+datasetNum+"-"+clustNum+"-"+j+"").bind("change",{world:j,dataset:datasetId,clusterId:clustNum-1},function(event){
-				console.log("world"+event.data.world+" dataset"+event.data.dataset+" cluster"+event.data.clusterId);
+				// $this will contain a reference to the checkbox
+				var $this = $(this);
+				if ($this.is(':checked')) {
+					//uncheck other non compatible buttons (same world same dataset)
+					$(".worldSelect"+(event.data.dataset)+"-"+(event.data.world)+"").prop("checked",false);
+				//check this one though
+				$this.prop("checked",true);
+					worlds[event.data.world].attachDataSet(datasets[event.data.dataset],event.data.dataset,event.data.clusterId);
+					$this.val(event.data.dataset);
+				} else {
+					worlds[event.data.world].detachDataSet($this.val());
+				}
 			});
 		}
 		$( "#buttonsClustAccordion"+datasetNum+"-"+clustNum+"" ).buttonset();
--- js/dataUpload.js
+++ js/dataUpload.js
@@ -43,11 +43,15 @@ function loaded(evt) {
 	$("#accordionData").append("<h3><a href='#'>Dataset #"+index+"</a></h3><div id='worldDataAccordion"+index+"'>");
 	$("#worldDataAccordion"+index+"").append("Show raw in world :<div id='worldSelectData"+index+"'></div>");
 	for(var i=1;i<=worlds.length;i++) {
-		$("#worldSelectData"+index+"").append("<input type='checkbox' id='worldSelectData"+index+""+i+"' /><label for='worldSelectData"+index+""+i+"'>world "+i+"</label>");
+		$("#worldSelectData"+index+"").append("<input type='checkbox' class='worldSelect"+(index)+"-"+(i)+"' id='worldSelectData"+index+""+i+"' /><label for='worldSelectData"+index+""+i+"'>world "+i+"</label>");
 		$("#worldSelectData"+index+""+i+"").bind("change",{world:i,dataset:index},function(event){
 			// $this will contain a reference to the checkbox
 			var $this = $(this);
 			if ($this.is(':checked')) {
+				//uncheck other non compatible buttons (same world same dataset)
+				$(".worldSelect"+(event.data.dataset-1)+"-"+(event.data.world-1)+"").prop("checked",false);
+				//check this one though
+				$this.prop("checked",true);
 				worlds[event.data.world-1].attachDataSet(datasets[event.data.dataset-1],event.data.dataset-1);
 				$this.val(event.data.dataset-1);
 			} else {
