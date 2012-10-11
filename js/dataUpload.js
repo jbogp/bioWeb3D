01286cc9a5c4633ff8f4ad32b104fb0862048052
Jean-Baptiste Pettit <jbpettit@vanilla.ebi.ac.uk>
2012-10-11 17:09:53 +0100

    11/10

--- js/dataUpload.js
+++ js/dataUpload.js
@@ -41,26 +41,30 @@ function loaded(evt) {
   //Adding dataset to the UI
 	//Create accordion
 	$("#accordionData").append("<h3><a href='#'>Dataset #"+index+"</a></h3><div id='worldDataAccordion"+index+"'>");
-	$("#worldDataAccordion"+index+"").append("Show raw in world :<div id='worldSelectData"+index+"'></div>");
+	$("#worldDataAccordion"+index+"").append("<div id='worldSelectData"+index+"'></div>");
 	for(var i=1;i<=worlds.length;i++) {
-		$("#worldSelectData"+index+"").append("<input type='checkbox' class='worldSelect"+(index)+"-"+(i)+"' id='worldSelectData"+index+""+i+"' /><label for='worldSelectData"+index+""+i+"'>world "+i+"</label>");
-		$("#worldSelectData"+index+""+i+"").bind("change",{world:i,dataset:index},function(event){
+		$("#worldSelectData"+index+"").append("<h2><a href=\"#\">world "+i+"</a></h2><div id='worldSelectData"+index+"-"+i+"'><input type=\"checkbox\" class='worldButtonData"+index+"-"+(i)+"' id='worldButtonData"+index+""+i+"' /><label for='worldButtonData"+index+""+i+"'>raw data</label><br /></div>");
+		$("#worldButtonData"+index+""+i+"").bind("change",{world:i,dataset:index},function(event){
 			// $this will contain a reference to the checkbox
 			var $this = $(this);
 			if ($this.is(':checked')) {
 				//uncheck other non compatible buttons (same world same dataset)
-				$(".worldSelect"+(event.data.dataset-1)+"-"+(event.data.world-1)+"").prop("checked",false);
+				$(".worldButtonData"+(event.data.dataset)+"-"+(event.data.world)+"").attr("checked",false);
 				//check this one though
-				$this.prop("checked",true);
+				$this.attr("checked",true);
+				$(".worldButtonData"+(event.data.dataset)+"-"+(event.data.world)+"").button("refresh");
 				worlds[event.data.world-1].attachDataSet(datasets[event.data.dataset-1],event.data.dataset-1);
 				$this.val(event.data.dataset-1);
+				//Clearing the cluster area
+				$("#clusters"+(event.data.dataset)+"-"+(event.data.world)+"").empty();
 			} else {
 				worlds[event.data.world-1].detachDataSet($this.val());
 			}
 		});
+		$('#worldButtonData'+index+''+i+'').button();
 	}
 	//create JqueryUI radio button
-	$( "#worldSelectData"+index+"" ).buttonset();
+	$( "#worldSelectData"+index+"" ).accordion({header: "h2", heightStyle: "content"});
 	
 	//Appening input file to import cluster data
 	$("#worldDataAccordion"+index+"").append("<div id='clustAccordion"+index+"'></div>");
