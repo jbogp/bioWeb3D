d0cd2fb0de2b0226c5d68f50af10f3358de07931
Jean-Baptiste Pettit <jbpettit@vanilla.ebi.ac.uk>
2012-11-07 17:12:05 +0000

    7/11

--- js/dataUpload.js
+++ js/dataUpload.js
@@ -47,9 +47,11 @@ function addDataSetUI(index) {
 	$("#accordionData").append("<h3><a href='#'>"+datasets[index-1].name+"</a></h3><div id='worldDataAccordion"+index+"'>");
 	$("#worldDataAccordion"+index+"").append("<div id='worldSelectData"+index+"'></div>");
 	for(var i=1;i<=worlds.length;i++) {
-		if(worlds[i-1].visible) {
+		if(worlds[i-1].visible && worlds[i-1].empty) {
 			$("#worldSelectData"+index+"").append("<h2><a href=\"#\">world "+i+"</a></h2><div id='worldSelectData"+index+"-"+i+"'><select id='worldButtonData"+index+""+i+"'><option class='noaction'>Select Data</option><option class='raw' selected>Raw Data</option></select></div>");
 			$("#worldButtonData"+index+""+i+"").bind("change",{world:i,dataset:index},selectDataHandler);
+			//Viewing dataset in visible worlds
+			worlds[i-1].attachDataSet(datasets[index-1],index-1);
 		}
 		else {
 			$("#worldSelectData"+index+"").append("<h2><a href=\"#\">world "+i+"</a></h2><div id='worldSelectData"+index+"-"+i+"'><select id='worldButtonData"+index+""+i+"'><option class='noaction'>Select Data</option><option class='raw'>Raw Data</option></select></div>");
@@ -68,12 +70,6 @@ function addDataSetUI(index) {
 	$("#accordionData").accordion({ header: "h3" , heightStyle: "content"});
 	$("#accordion").accordion( "refresh" );
 	consoleMess("Loaded dataset \""+datasets[index-1].name+"\" containing "+datasets[index-1].points.length+" points");
-	  //Viewing dataset in visible worlds
-		for(var i=0;i<worlds.length;i++) {
-			if(worlds[i].visible) {
-				worlds[i].attachDataSet(datasets[index-1],index-1);
-			}
-		}
 }
 
 function errorHandler(evt) {
